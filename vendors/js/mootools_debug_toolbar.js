326a96472014b84a480ed6d2ce0465e045b655d8
Mark Story <mark@mark-story.com>
2008-11-03 22:42:55 -0500

    Adding mootools compatible javascript

--- vendors/js/mootools_debug_toolbar.js
+++ vendors/js/mootools_debug_toolbar.js
@@ -2,6 +2,8 @@
 /**
  * Debug Toolbar Javascript.  Mootools 1.2 compatible
  *
+ * Requires Class, Event, Element, and Selectors 
+ *
  *
  * PHP versions 4 and 5
  *
@@ -25,51 +27,67 @@
  * @license			http://www.opensource.org/licenses/mit-license.php The MIT License
  */
 window.addEvent('domready', function() {
-	DebugKit.Toolbar();
-	DebugKit.NeatArray();
+	new DebugKit();
 });
-var DebugKit = {};
+var DebugKit = new Class({
+	initialize : function() {
+		this.neatArray();
+		this.toolbar();
+	},
 /**
  * Create all behaviors for neat array elements
- *
  */
-DebugKit.NeatArray = function() {
+	neatArray : function() {
+		$$('#debug-kit-toolbar .neat-array li').each(function(listItem) {
+			var subUl = listItem.getElement('ul');
+			if (subUl) {
+				listItem.addClass('expandable').addClass('collapsed');
+				subUl.setStyle('display', 'none').set('state', 'closed');
+				listItem.addEvent('click', function(event) {
+					event.stop();
+					this.toggleClass('expanded').toggleClass('collapsed');
+					if (subUl.get('state') == 'closed') {
+						subUl.setStyle('display', 'block').set('state', 'open');
+					} else {
+						subUl.setStyle('display', 'none').set('state', 'closed');
+					}
+				}) 
+			}
+		});
+	},
 
-}
+	/**
+	 * Add behavior for toolbar buttons
+	 */
+	toolbar : function() {
+		var tabCollection = $$('#debug-kit-toolbar li > div');
 
-/**
- * Add behavior for toolbar buttons
- *
- */
-DebugKit.Toolbar = function() {
-	var tabCollection = $$('#debug-kit-toolbar li > div');
-	 
-	$$('#debug-kit-toolbar .panel-tab a').addEvent('click', function(event) {
-		event.stop();
-		var buttonId = this.hash.substring(1, this.hash.length) + '-tab';
-		var targetPanel = $(buttonId);
-		if (!targetPanel) return;
-		if (targetPanel.hasClass('active')) {
-			tabCollection.removeClass('active').setStyle('display', 'none');
-		} else {
-			tabCollection.setStyle('display', 'none').removeClass('active');
-			targetPanel.addClass('active').setStyle('display', 'block');
-		}
-		
-	});
-	
-	//enable hiding of toolbar.
-	var panelButtons = $$('#debug-kit-toolbar .panel-tab:not(.panel-tab.icon)');
-	var toolbarHide = $('hide-toolbar').set('state', 'open');
-	toolbarHide.addEvent('click', function(event) {
-		event.stop();
-		var state = this.get('state');
-		if (state == 'open') {
-			panelButtons.setStyle('display', 'none');
-			this.set('state', 'closed')
-		} else {
-			panelButtons.setStyle('display');
-			this.set('state', 'open');
-		}
-	});
-}
+		$$('#debug-kit-toolbar .panel-tab a').addEvent('click', function(event) {
+			event.stop();
+			var buttonId = this.hash.substring(1, this.hash.length) + '-tab';
+			var targetPanel = $(buttonId);
+			if (!targetPanel) return;
+			if (targetPanel.hasClass('active')) {
+				tabCollection.removeClass('active').setStyle('display', 'none');
+			} else {
+				tabCollection.setStyle('display', 'none').removeClass('active');
+				targetPanel.addClass('active').setStyle('display', 'block');
+			}
+		});
+
+		//enable hiding of toolbar.
+		var panelButtons = $$('#debug-kit-toolbar .panel-tab:not(.panel-tab.icon)');
+		var toolbarHide = $('hide-toolbar').set('state', 'open');
+		toolbarHide.addEvent('click', function(event) {
+			event.stop();
+			var state = this.get('state');
+			if (state == 'open') {
+				panelButtons.setStyle('display', 'none');
+				this.set('state', 'closed')
+			} else {
+				panelButtons.setStyle('display');
+				this.set('state', 'open');
+			}
+		});
+	}
+});

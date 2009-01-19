3da1fc0ddb3a3db9e9fa8db1b910548fc3f1e41b
Mark Story <mark@mark-story.com>
2009-01-19 08:43:07 -0500

    Adding Cookie handlers to JS

--- vendors/js/js_debug_toolbar.js
+++ vendors/js/js_debug_toolbar.js
@@ -217,4 +217,55 @@ DebugKit.install = function() {
 	}
 }
 
+
+/** 
+ * Utility functions for debugKit Js
+ */
+DebugKit.Util = {};
+
+DebugKit.Util.Cookie = function() {
+	var cookieLife = 60;
+/**
+ * Write to cookie
+ * @param [string] name Name of cookie to write.
+ * @param [mixed] value Value to write to cookie.
+ */
+	this.write = function (name, value) {
+		var date = new Date();
+		date.setTime(date.getTime() + (cookieLife * 24 * 60 * 60 * 1000));
+		var expires = "; expires=" + date.toGMTString();
+		document.cookie = name + "=" + value + expires + "; path=/";
+		return true;
+	}
+/**
+ * Read from the cookie
+ * @param [string] name Name of cookie to read.
+ */	
+	this.read = function (name) {
+		name = name + '=';
+		var cookieJar = document.cookie.split(';');
+		for (var i = 0; i < cookieJar.length; i++) {
+			var chips = cookieJar[i];
+			//trim leading spaces
+			while (chips.charAt(0) == ' ') {
+				chips = chips.substring(1, chips.length);
+			}
+			if (chips.indexOf(name) == 0) {
+				return chips.substring(name.length, chips.length)
+			}
+		}
+		return false;
+	}
+/**
+ * Delete a cookie by name.
+ */
+	this.delete = function (name) {
+		var date = new Date();
+		date.setFullYear(2000,0,1);
+		var expires = " ; expires=" + date.toGMTString();
+		document.cookie = name + "=" + expires + "; path=/";
+	}
+}
+
+
 DebugKit.install();

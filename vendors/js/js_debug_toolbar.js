210e9a1bf91c2339eb3dfe6c12582c0bc4af7a3c
Mark Story <mark@mark-story.com>
2009-03-18 00:58:58 -0400

    Basic toolbar features refactored out.

--- vendors/js/js_debug_toolbar.js
+++ vendors/js/js_debug_toolbar.js
@@ -25,7 +25,6 @@
  * @lastmodified  $Date$
  * @license       http://www.opensource.org/licenses/mit-license.php The MIT License
  */
-
 var DEBUGKIT = function () {
 	var undef;
 	return {
@@ -36,14 +35,302 @@ var DEBUGKIT = function () {
 			}
 			return this[newmodule];
 		}
-	}
+	};
 }() ;
 
+DEBUGKIT.module('loader');
+DEBUGKIT.loader = function () {
+	return {
+		//list of methods to run on startup.
+		_startup : [],
+	
+		//register a new method to be run on dom ready.
+		register : function (method, context) {
+			if (context !== undefined) {
+				var bound = function () {
+					return method.apply(context);
+				}
+				this._startup.push(bound);
+				return;
+			}
+			this._startup.push(method);
+		},
+
+		init : function () {
+			for (var i = 0; i < this._startup.length; i++) {
+				this._startup[i]();
+			}
+		}
+	};
+}();
+
+//Util module and Element utility class.
+DEBUGKIT.module('Util');
+DEBUGKIT.Util.Element = {
+	hasClass : function (element, className) {
+		if (!element.className) {
+			return false;
+		}
+		return element.className.match(new RegExp(className));
+	},
+
+	addClass : function (element, className) {
+		if (!element.className) {
+			element.className = '';
+		}
+		element.className = element.className.replace(/^(.*)$/, '$1 ' + className);
+	},
+
+	removeClass : function (element, className) {
+		if (!element.className) {
+			return false;
+		} 
+		element.className = element.className.replace(new RegExp(' ?(' + className +') ?'), '');
+	},
+
+	show : function (element) {
+		element.style.display = 'block';
+	},
+
+	hide : function (element) {
+		element.style.display = 'none';
+	},
+	
+	addEvent :function(element, type, handler, capture) {
+		capture = (capture === undefined) ? false : capture;
+		if (element.addEventListener) {
+			element.addEventListener(type, handler, capture);
+		} else if (element.attachEvent) {
+			type = 'on' + type;
+			element.attachEvent(type, handler);
+		} else {
+			type = 'on' + type;
+			element[type] = handler;
+		}
+	}
+};
+
+//Cookie utility
+DEBUGKIT.Util.Cookie = function() {
+	var cookieLife = 60;
+	return {
+		/**
+		 * Write to cookie
+		 * @param [string] name Name of cookie to write.
+		 * @param [mixed] value Value to write to cookie.
+		 */
+			write : function (name, value) {
+				var date = new Date();
+				date.setTime(date.getTime() + (cookieLife * 24 * 60 * 60 * 1000));
+				var expires = "; expires=" + date.toGMTString();
+				document.cookie = name + "=" + value + expires + "; path=/";
+				return true;
+			},
+		/**
+		 * Read from the cookie
+		 * @param [string] name Name of cookie to read.
+		 */
+		read : function (name) {
+			name = name + '=';
+			var cookieJar = document.cookie.split(';');
+			for (var i = 0; i < cookieJar.length; i++) {
+				var chips = cookieJar[i];
+				//trim leading spaces
+				while (chips.charAt(0) == ' ') {
+					chips = chips.substring(1, chips.length);
+				}
+				if (chips.indexOf(name) == 0) {
+					return chips.substring(name.length, chips.length);
+				}
+			}
+			return false;
+		},
+		/**
+		 * Delete a cookie by name.
+		 */
+		del : function (name) {
+			var date = new Date();
+			date.setFullYear(2000,0,1);
+			var expires = " ; expires=" + date.toGMTString();
+			document.cookie = name + "=" + expires + "; path=/";
+		}
+	};
+}();
+
+
+//Basic toolbar module.
 DEBUGKIT.module('toolbar');
+DEBUGKIT.toolbar = function () {
+	var Request = DEBUGKIT.Request,
+		Element = DEBUGKIT.Util.Element,
+		Cookie = DEBUGKIT.Util.Cookie,
+		toolbarHidden = false;
+
+	return {
+		elements : {},
+		panels : {},
+
+		init : function () {
+			var i, element, lists, index;
+			this.elements.toolbar = document.getElementById('debug-kit-toolbar');
+
+			if (this.elements.toolbar === undefined) {
+				throw('Toolbar not found, make sure you loaded it.');
+			}
+			
+			for (i in this.elements.toolbar.childNodes) {
+				element = this.elements.toolbar.childNodes[i];
+				if (element.nodeName && element.id === 'panel-tabs') {
+					this.elements.panel = element;
+					break;
+				}
+			}
+
+			for (i in this.elements.panel.childNodes) {
+				element = this.elements.panel.childNodes[i];
+				if (Element.hasClass(element, 'panel-tab')) {
+					this.addPanel(element);
+				}
+			}
+			//lists = document.getElementsByTagName('ul');
+			//this.makeNeatArray(lists);
+
+			this.deactivatePanel(true);
+			var toolbarState = Cookie.read('toolbarDisplay');
+			if (toolbarState != 'block') {
+				toolbarHidden = false;
+				this.toggleToolbar();
+			}
+		},
+		/**
+		 * Add a panel to the toolbar
+		 */
+		addPanel : function (tab, callback) {
+			if (!tab.nodeName || tab.nodeName.toUpperCase() !== 'LI') {
+				throw ('Toolbar not found, make sure you loaded it.');
+			}
+			var panel = {
+				id : false,
+				element : tab,
+				callback : callback,
+				button : undefined,
+				content : undefined,
+				active : false
+			};
+			for (var i in tab.childNodes) {
+				var element = tab.childNodes[i],
+					tag = element.nodeName ? element.nodeName.toUpperCase() : false;
+				if (tag === 'A') {
+					panel.id = element.hash.replace(/^#/, '');
+					panel.button = element;
+				} else if (tag === 'DIV') {
+					panel.content = element;
+				}
+			}
+			if (!panel.id) {
+				throw('invalid element');
+			}
+
+			if (panel.button.id && panel.button.id === 'hide-toolbar') {
+				panel.callback = this.toggleToolbar;
+			}
+
+			var callbackName = "activate" + panel.id.replace('-tab', '');
+			if (this[callbackName] !== undefined) {
+				this[callbackName](panel);
+			}
+
+			if (panel.callback !== undefined) {
+				Element.addEvent(panel.button, 'click', function(event) {
+					event = event || window.event;
+					event.preventDefault();
+					return panel.callback();
+				});
+			} else {
+				var self = this;
+				Element.addEvent(panel.button, 'click', function(event) {
+					event = event || window.event;
+					event.preventDefault();
+					return self.togglePanel(panel.id);
+				});
+			}
+			this.panels[panel.id] = panel;
+			return panel.id;
+		},
+		
+		toggleToolbar : function () {
+			var display = toolbarHidden ? 'block' : 'none';
+			for (var i in this.panels) {
+				var panel = this.panels[i];
+				if (panel.content !== undefined) {
+					panel.element.style.display = display;
+					Cookie.write('toolbarDisplay', display);
+				}
+			}
+			toolbarHidden = !toolbarHidden;
+			return false;
+		},
+
+	/**
+	 * Toggle a panel
+	 */
+		togglePanel : function (id) {
+			if (this.panels[id] && this.panels[id].active) {
+				this.deactivatePanel(true);
+			} else {
+				this.deactivatePanel(true);
+				this.activatePanel(id);
+			}
+		},
+
+	/**
+	 * Make a panel active.
+	 */
+		activatePanel : function (id, unique) {
+			if (this.panels[id] !== undefined && !this.panels[id].active) {
+				var panel = this.panels[id];
+				if (panel.content !== undefined) {
+					Element.show(panel.content);
+				}
+				Element.addClass(panel.button, 'active');
+				panel.active = true;
+				return true;
+			}
+			return false;
+		},
+
+	/**
+	 * Deactivate a panel.  use true to hide all panels.
+	 */
+		deactivatePanel : function (id) {
+			if (id === true) {
+				for (var i in this.panels) {
+					this.deactivatePanel(i);
+				}
+				return true;
+			}
+			if (this.panels[id] !== undefined) {
+				var panel = this.panels[id];
+				if (panel.content !== undefined) {
+					Element.hide(panel.content);
+				}
+				Element.removeClass(panel.button, 'active');
+				panel.active = false;
+				return true;
+			}
+			return false;
+		}
+
+	};
+}();
+DEBUGKIT.loader.register(DEBUGKIT.toolbar.init, DEBUGKIT.toolbar);
+
+window.addEventListener('DOMContentLoaded', function () {
+	DEBUGKIT.loader.init();
+}, false);
 
 
-
-
+/*
 var DebugKit = function (id) {
 	var elements = {},
 		panels = {},
@@ -90,7 +377,7 @@ var DebugKit = function (id) {
 	};
 /**
  * Add a panel to the toolbar
- */
+ * /
 	this.addPanel = function (tab, callback) {
 		if (!tab.nodeName || tab.nodeName.toUpperCase() !== 'LI') {
 			throw('Toolbar not found, make sure you loaded it.');
@@ -144,7 +431,7 @@ var DebugKit = function (id) {
 	};
 /**
  * Hide/show the toolbar (minimize cake)
- */	
+ * /	
 	this.toggleToolbar = function () {
 		var display = toolbarHidden ? 'block' : 'none';
 		for (var i in panels) {
@@ -159,7 +446,7 @@ var DebugKit = function (id) {
 	};
 /**
  * Toggle a panel
- */
+ * /
 	this.togglePanel = function (id) {
 		if (panels[id] && panels[id].active) {
 			this.deactivatePanel(true);
@@ -170,7 +457,7 @@ var DebugKit = function (id) {
 	};
 /**
  * Make a panel active.
- */
+ * /
 	this.activatePanel = function (id, unique) {
 		if (panels[id] !== undefined && !panels[id].active) {
 			var panel = panels[id];
@@ -185,7 +472,7 @@ var DebugKit = function (id) {
 	};
 /**
  * Deactivate a panel.  use true to hide all panels.
- */
+ * /
 	this.deactivatePanel = function (id) {
 		if (id === true) {
 			for (var i in panels) {
@@ -207,7 +494,7 @@ var DebugKit = function (id) {
 /**
  * Activate history panel.
  * adds events to all the button
- */	
+ * /	
 	this.activatehistory = function (panel) {
 		var anchors = panel.element.getElementsByTagName('A'),
 			historyLinks = [], 
@@ -224,7 +511,7 @@ var DebugKit = function (id) {
 		/**
 		 * Private methods to handle JSON response and insertion of 
 		 * new content.
-		 */
+		 * /
 		var switchHistory = function (response) {
 			try {
 				var responseJson = eval( '(' + response.response.text + ')');
@@ -265,7 +552,7 @@ var DebugKit = function (id) {
 		
 		/**
 		 * Private method to handle restoration to current request.
-		 */
+		 * /
 		var restoreCurrentState = function () {
 			var id, i, panelContent, tag;
 			
@@ -331,7 +618,7 @@ var DebugKit = function (id) {
 	};
 /**
  * Add neat array functionality.
- */
+ * /
 	var neatArray = function (list) {
 		if (!list.className.match(/depth-0/)) {
 			var item = list.parentNode;
@@ -363,7 +650,7 @@ var DebugKit = function (id) {
 
 /** 
  * Utility functions for debugKit Js
- */
+ * /
 DebugKit.Util = {};
 
 DebugKit.Util.Cookie = function() {
@@ -372,7 +659,7 @@ DebugKit.Util.Cookie = function() {
  * Write to cookie
  * @param [string] name Name of cookie to write.
  * @param [mixed] value Value to write to cookie.
- */
+ * /
 	this.write = function (name, value) {
 		var date = new Date();
 		date.setTime(date.getTime() + (cookieLife * 24 * 60 * 60 * 1000));
@@ -383,7 +670,7 @@ DebugKit.Util.Cookie = function() {
 /**
  * Read from the cookie
  * @param [string] name Name of cookie to read.
- */	
+ * /	
 	this.read = function (name) {
 		name = name + '=';
 		var cookieJar = document.cookie.split(';');
@@ -401,7 +688,7 @@ DebugKit.Util.Cookie = function() {
 	};
 /**
  * Delete a cookie by name.
- */
+ * /
 	this.del = function (name) {
 		var date = new Date();
 		date.setFullYear(2000,0,1);
@@ -412,7 +699,7 @@ DebugKit.Util.Cookie = function() {
 
 /**
  * Cross browser domready handler.
- */
+ * /
 DebugKit.Util.domready = function(callback) {
 	if (document.addEventListener) {
 		return document.addEventListener("DOMContentLoaded", callback, false);
@@ -442,7 +729,7 @@ DebugKit.Util.domready = function(callback) {
 
 /**
  * Cross browser event registration.
- */
+ * /
 DebugKit.Util.addEvent = function(element, type, handler, capture) {
 	capture = (capture === undefined) ? false : capture;
 	if (element.addEventListener) {
@@ -458,7 +745,7 @@ DebugKit.Util.addEvent = function(element, type, handler, capture) {
 
 /**
  * Simple Element manipulation shortcuts.
- */
+ * /
 DebugKit.Util.Element = {
 	hasClass : function (element, className) {
 		if (!element.className) {
@@ -489,7 +776,7 @@ DebugKit.Util.Element = {
  * Object merge takes any number of arguments and glues them together
  * @param [Object] one first object
  * @return object 
- */
+ * /
 DebugKit.prototype.merge = function() {
 	var out = {};
 	for (var i = 0; i < arguments.length; i++) {
@@ -505,7 +792,7 @@ DebugKit.prototype.merge = function() {
 
 /**
  * Simple wrapper for XmlHttpRequest objects.
- */
+ * /
 DebugKit.prototype.Request = function (options) {
 	var _defaults = {
 		onComplete : function (){},
@@ -515,7 +802,7 @@ DebugKit.prototype.Request = function (options) {
 		async : true,
 		headers : {
 			'X-Requested-With': 'XMLHttpRequest',
-			'Accept': 'text/javascript, text/html, application/xml, text/xml, */*'
+			'Accept': 'text/javascript, text/html, application/xml, text/xml, * /*'
 		}
 	};
 
@@ -576,7 +863,7 @@ DebugKit.prototype.Request.prototype.onReadyStateChange = function (){
 };
 /**
  * Creates cross-broswer XHR object used for requests
- */
+ * /
 DebugKit.prototype.Request.prototype.createObj = function(){
 	var request = null;
 	try {
@@ -599,3 +886,5 @@ DebugKit.prototype.Request.prototype.createObj = function(){
 DebugKit.Util.domready(function() {
 	window.DebugKit = new DebugKit();
 });
+
+*/

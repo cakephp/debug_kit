3c6ece4b5785dfa76496c37fb6223d17c03fce59
Mark Story <mark@mark-story.com>
2009-12-20 00:32:13 -0500

    Removing unneeded import.

--- controllers/components/toolbar.php
+++ controllers/components/toolbar.php
@@ -546,7 +546,6 @@ class SqlLogPanel extends DebugPanel {
 		if (!class_exists('ConnectionManager')) {
 			return array();
 		}
-		App::import('Core', 'Xml');
 		$queryLogs = array();
 
 		$dbConfigs = ConnectionManager::sourceList();

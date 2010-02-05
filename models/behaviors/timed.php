3ed47a0b3b0a81737b49db376b920988391289a2
Mark Story <mark@mark-story.com>
2010-02-04 22:53:15 -0500

    Adding doc blocks and test cases for the TimedBehavior.

--- models/behaviors/timed.php
+++ models/behaviors/timed.php
@@ -16,34 +16,36 @@
  * @subpackage    debug_kit.models.behaviors
  * @since         DebugKit 1.3
  * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
- **/
+ */
 
 class TimedBehavior extends ModelBehavior {
 
 /**
-* Behavior settings
-* 
-* @access public
-* @var array
-*/
-	var $settings = array(); 
+ * Behavior settings
+ *
+ * @access public
+ * @var array
+ */
+	var $settings = array();
 
 /**
-* Default setting values
-*
-* @access private
-* @var array
-*/ 	
+ * Default setting values
+ *
+ * @access private
+ * @var array
+ */
 	var $_defaults = array();
 
 /**
+ * Setup the behavior and import required classes.
+ *
  * @param object $Model Model using the behavior
  * @param array $settings Settings to override for model.
  * @access public
  * @return void
  */
 	function setup(&$Model, $settings = null) {
-		if(!class_exists('DebugKitDebugger')){
+		if (!class_exists('DebugKitDebugger')){
 			App::import('Vendor', 'DebugKit.DebugKitDebugger');
 		}
 		if (is_array($settings)) {
@@ -53,26 +55,52 @@ class TimedBehavior extends ModelBehavior {
 		}
 	}
 
+/**
+ * beforeFind, starts a timer for a find operation.
+ *
+ * @param Model $Model
+ * @param array $queryData Array of query data (not modified)
+ * @return boolean true
+ */
 	function beforeFind(&$Model, $queryData){
 		DebugKitDebugger::startTimer($Model->alias . '_find', $Model->alias . '->find()');
 		return true;
 	}
 
+/**
+ * afterFind, stops a timer for a find operation.
+ *
+ * @param Model $Model
+ * @param array $results Array of results
+ * @return boolean true.
+ */
 	function afterFind(&$Model, $results){
 		DebugKitDebugger::stopTimer($Model->alias . '_find');
 		return true;
 	}
 
+/**
+ * beforeSave, starts a time before a save is initiated.
+ *
+ * @param Model $Model
+ * @return boolean true
+ */
 	function beforeSave(&$Model){
 		DebugKitDebugger::startTimer($Model->alias . '_save', $Model->alias . '->save()');
 		return true;
 	}
 
+/**
+ * afterSave, stop the timer started from a save.
+ *
+ * @param string $Model
+ * @param string $created
+ * @return void
+ */
 	function afterSave(&$Model, $created) {
 		DebugKitDebugger::stopTimer($Model->alias . '_save');
 		return true;
 	}
  }
-
 
 ?>
---
+++ tests/cases/behaviors/timed.test.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * DebugKit TimedBehavior test case
+ *
+ * PHP versions 4 and 5
+ *
+ * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
+ * Copyright 2005-2010, Cake Software Foundation, Inc. (http://cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright 2005-2010, Cake Software Foundation, Inc. (http://cakefoundation.org)
+ * @link          http://cakephp.org
+ * @package       debug_kit
+ * @subpackage    debug_kit.models.behaviors
+ * @since         DebugKit 1.3
+ * @license       MIT License (http://www.opensource.org/licenses/mit-license.php)
+ */
+App::import('Vendor', 'DebugKit.DebugKitDebugger');
+
+class TimedBehaviorTestCase extends CakeTestCase {
+
+	var $fixtures = array('core.article');
+/**
+ * startTest callback
+ *
+ * @return void
+ */
+	function startTest() {
+		$this->Article =& new Model(array('ds' => 'test_suite', 'table' => 'Articles', 'name' => 'Article'));
+		$this->Article->Behaviors->attach('DebugKit.Timed');
+	}
+
+/**
+ * end a test
+ *
+ * @return void
+ */
+	function endTest() {
+		unset($this->Article);
+		ClassRegistry::flush();
+		DebugKitDebugger::clearTimers();
+	}
+
+/**
+ * test find timers
+ *
+ * @return void
+ */
+	function testFindTimers() {
+		$timers = DebugKitDebugger::getTimers(false);
+		$this->assertEqual(count($timers), 1);
+
+		$this->Article->find('all');
+		$result = DebugKitDebugger::getTimers(false);
+		$this->assertEqual(count($result), 2);
+		
+		$this->Article->find('all');
+		$result = DebugKitDebugger::getTimers(false);
+		$this->assertEqual(count($result), 3);
+	}
+
+/**
+ * test save timers
+ *
+ * @return void
+ */
+	function testSaveTimers() {
+		$timers = DebugKitDebugger::getTimers(false);
+		$this->assertEqual(count($timers), 1);
+
+		$this->Article->save(array('user_id' => 1, 'title' => 'test', 'body' => 'test'));
+		$result = DebugKitDebugger::getTimers(false);
+		$this->assertEqual(count($result), 2);
+	}
+}

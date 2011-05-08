e17ffe08fc575fbb9e7fd9ea042565bdee578537
jonbradley <iamjonbradley@gmail.com>
2011-05-07 21:06:56 -0400

    debug kit is now working with Cake 2.0

--- View/Helper/HtmlToolbarHelper.php
+++ View/Helper/HtmlToolbarHelper.php
@@ -150,7 +150,7 @@ class HtmlToolbarHelper extends ToolbarHelper {
 		if (preg_match('#</head>#', $view->output)) {
 			$view->output = preg_replace('#</head>#', $head . "\n</head>", $view->output, 1);
 		}
-		$toolbar = $this->_View->element('debug_toolbar', array('plugin' => 'debug_kit', 'disableTimer' => true));
+		$toolbar = $view->element('debug_toolbar', array(), array('plugin' => 'debug_kit', 'disableTimer' => true));
 		if (preg_match('#</body>#', $view->output)) {
 			$view->output = preg_replace('#</body>#', $toolbar . "\n</body>", $view->output, 1);
 		}

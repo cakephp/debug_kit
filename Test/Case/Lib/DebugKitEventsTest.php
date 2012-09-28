a5cac9aa2b3fbea684c5ce7f2b0d80463ece5b35
dogmatic69 <dogmatic69@gmail.com>
2012-09-29 00:39:37 +0100

    adding tests

---
+++ Test/Case/Lib/DebugKitEventsTest.php
@@ -0,0 +1,15 @@
+<?php
+App::uses('DebugKitEvents', 'DebugKit.Lib');
+class DebugKitEventsTest extends CakeTestCase {
+	public function setUp() {
+		parent::setUp();
+	}
+
+	public function tearDown() {
+		parent::tearDown();
+	}
+
+	public function testSomething() {
+
+	}
+}

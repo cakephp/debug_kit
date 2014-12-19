faeddb10c7739716b46d2a7493e5a234367e8293
Bryan Crowe <bryan@bryan-crowe.com>
2014-12-18 22:36:06 -0500

    Run PSR-2 fixers

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -49,52 +49,52 @@ mb_internal_encoding('UTF-8');
 
 Configure::write('debug', true);
 Configure::write('App', [
-	'namespace' => 'App',
-	'encoding' => 'UTF-8',
-	'base' => false,
-	'baseUrl' => false,
-	'dir' => 'src',
-	'webroot' => 'webroot',
-	'www_root' => APP . 'webroot',
-	'fullBaseUrl' => 'http://localhost',
-	'imageBaseUrl' => 'img/',
-	'jsBaseUrl' => 'js/',
-	'cssBaseUrl' => 'css/',
-	'paths' => [
-		'plugins' => [APP . 'Plugin' . DS],
-		'templates' => [APP . 'Template' . DS]
-	]
+    'namespace' => 'App',
+    'encoding' => 'UTF-8',
+    'base' => false,
+    'baseUrl' => false,
+    'dir' => 'src',
+    'webroot' => 'webroot',
+    'www_root' => APP . 'webroot',
+    'fullBaseUrl' => 'http://localhost',
+    'imageBaseUrl' => 'img/',
+    'jsBaseUrl' => 'js/',
+    'cssBaseUrl' => 'css/',
+    'paths' => [
+        'plugins' => [APP . 'Plugin' . DS],
+        'templates' => [APP . 'Template' . DS]
+    ]
 ]);
 Configure::write('Session', [
-	'defaults' => 'php'
+    'defaults' => 'php'
 ]);
 
 Cache::config([
-	'_cake_core_' => [
-		'engine' => 'File',
-		'prefix' => 'cake_core_',
-		'serialize' => true
-	],
-	'_cake_model_' => [
-		'engine' => 'File',
-		'prefix' => 'cake_model_',
-		'serialize' => true
-	],
-	'default' => [
-		'engine' => 'File',
-		'prefix' => 'default_',
-		'serialize' => true
-	]
+    '_cake_core_' => [
+        'engine' => 'File',
+        'prefix' => 'cake_core_',
+        'serialize' => true
+    ],
+    '_cake_model_' => [
+        'engine' => 'File',
+        'prefix' => 'cake_model_',
+        'serialize' => true
+    ],
+    'default' => [
+        'engine' => 'File',
+        'prefix' => 'default_',
+        'serialize' => true
+    ]
 ]);
 
 // Ensure default test connection is defined
 if (!getenv('db_dsn')) {
-	putenv('db_dsn=sqlite://' . TMP . 'debug_kit_test.sqlite');
+    putenv('db_dsn=sqlite://' . TMP . 'debug_kit_test.sqlite');
 }
 
 $config = [
-	'url' => getenv('db_dsn'),
-	'timezone' => 'UTC',
+    'url' => getenv('db_dsn'),
+    'timezone' => 'UTC',
 ];
 
 // Use the test connection for 'debug_kit' as well.
@@ -103,16 +103,16 @@ ConnectionManager::config('test_debug_kit', $config);
 
 
 Log::config([
-	'debug' => [
-		'engine' => 'Cake\Log\Engine\FileLog',
-		'levels' => ['notice', 'info', 'debug'],
-		'file' => 'debug',
-	],
-	'error' => [
-		'engine' => 'Cake\Log\Engine\FileLog',
-		'levels' => ['warning', 'error', 'critical', 'alert', 'emergency'],
-		'file' => 'error',
-	]
+    'debug' => [
+        'engine' => 'Cake\Log\Engine\FileLog',
+        'levels' => ['notice', 'info', 'debug'],
+        'file' => 'debug',
+    ],
+    'error' => [
+        'engine' => 'Cake\Log\Engine\FileLog',
+        'levels' => ['warning', 'error', 'critical', 'alert', 'emergency'],
+        'file' => 'error',
+    ]
 ]);
 
 Plugin::load('DebugKit', ['path' => ROOT, 'bootstrap' => true]);

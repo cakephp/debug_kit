fffe1173ba7f9d61b38e724396787d31ba8c63f1
Mark Story <mark@mark-story.com>
2015-12-29 18:23:55 -0500

    Move shell tasks in to the correct locations.

--- src/Shell/BenchmarkShell.php
+++ src/Shell/BenchmarkShell.php
@@ -11,7 +11,7 @@
  * @since         DebugKit 1.0
  * @license       http://www.opensource.org/licenses/mit-license.php MIT License
  */
-namespace DebugKit\Console\Command;
+namespace DebugKit\Shell;
 
 use Cake\Console\Shell;
 use Cake\Utility\Text;

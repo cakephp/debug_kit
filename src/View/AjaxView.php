3c98c9269c8e136cb66ea7b78afa16475c09de07
Mark Story <mark@mark-story.com>
2016-04-15 21:42:25 -0400

    Fix PHPCS errors.

--- src/View/AjaxView.php
+++ src/View/AjaxView.php
@@ -18,7 +18,8 @@ use Cake\View\View;
 
 /**
  * A view class that is used for AJAX responses.
- * Currently only switches the default layout and sets the response type -
+ *
+ * Currently only switches the default layout and sets the response type
  * which just maps to text/html by default.
  */
 class AjaxView extends View

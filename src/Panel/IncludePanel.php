e1d87ea5bb33db94366b77e9fc0f864a850a7c0f
Mark Story <mark@mark-story.com>
2016-06-02 22:14:30 -0400

    Fix Include Panel on Windows.
    * Fix incorrectly group files on windows.
    * Rename 'core' to 'cake'. I feel this name better represents what the
      files are. core is a bit ambiguous.
    
    Refs #424

--- src/Panel/IncludePanel.php
+++ src/Panel/IncludePanel.php
@@ -61,7 +61,7 @@ class IncludePanel extends DebugPanel
      */
     protected function _prepare()
     {
-        $return = ['core' => [], 'app' => [], 'plugins' => []];
+        $return = ['cake' => [], 'app' => [], 'plugins' => []];
 
         foreach (get_included_files() as $file) {
             $pluginName = $this->_isPluginFile($file);
@@ -70,14 +70,14 @@ class IncludePanel extends DebugPanel
                 $return['plugins'][$pluginName][$this->_getFileType($file)][] = $this->_niceFileName($file, $pluginName);
             } elseif ($this->_isAppFile($file)) {
                 $return['app'][$this->_getFileType($file)][] = $this->_niceFileName($file, 'app');
-            } elseif ($this->_isCoreFile($file)) {
-                $return['core'][$this->_getFileType($file)][] = $this->_niceFileName($file, 'core');
+            } elseif ($this->_isCakeFile($file)) {
+                $return['cake'][$this->_getFileType($file)][] = $this->_niceFileName($file, 'cake');
             }
         }
 
         $return['paths'] = $this->_includePaths();
 
-        ksort($return['core']);
+        ksort($return['cake']);
         ksort($return['plugins']);
         ksort($return['app']);
         return $return;
@@ -97,12 +97,12 @@ class IncludePanel extends DebugPanel
     }
 
     /**
-     * Check if a path is part of cake core
+     * Check if a path is part of CakePHP
      *
      * @param string $file File to check
      * @return bool
      */
-    protected function _isCoreFile($file)
+    protected function _isCakeFile($file)
     {
         return strstr($file, CAKE);
     }
@@ -140,7 +140,7 @@ class IncludePanel extends DebugPanel
      * @param string $file File to check
      * @param string $type The file type
      *  - app for app files
-     *  - core for core files
+     *  - cake for cake files
      *  - PluginName for the name of a plugin
      * @return bool
      */
@@ -150,8 +150,8 @@ class IncludePanel extends DebugPanel
             case 'app':
                 return str_replace(APP, 'APP/', $file);
 
-            case 'core':
-                return str_replace(CAKE, 'CORE/', $file);
+            case 'cake':
+                return str_replace(CAKE, 'CAKE/', $file);
 
             default:
                 return str_replace($this->_pluginPaths[$type], $type . '/', $file);
@@ -167,7 +167,7 @@ class IncludePanel extends DebugPanel
     protected function _getFileType($file)
     {
         foreach ($this->_fileTypes as $type) {
-            if (stripos($file, '/' . $type . '/') !== false) {
+            if (stripos($file, DIRECTORY_SEPARATOR . $type . DIRECTORY_SEPARATOR) !== false) {
                 return $type;
             }
         }

c3306fc0ad7c082b25e6e78876e5147f2329c19a
José Lorenzo Rodríguez <lorenzo@users.noreply.github.com>
2017-01-29 12:49:35 +0100

    Fixed doc block

--- src/Mailer/MailPreview.php
+++ src/Mailer/MailPreview.php
@@ -8,7 +8,6 @@
  *
  * @copyright     Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
  * @link          http://cakephp.org CakePHP(tm) Project
- * @since         3.3
  * @license       http://www.opensource.org/licenses/mit-license.php MIT License
  */
 namespace DebugKit\Mailer;

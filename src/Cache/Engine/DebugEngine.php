c9f613efe5977f4809e1af573959ddede82f0c28
Mark Story <mark@mark-story.com>
2018-02-02 14:36:31 -0500

    Fix comment styles.

--- src/Cache/Engine/DebugEngine.php
+++ src/Cache/Engine/DebugEngine.php
@@ -260,27 +260,27 @@ class DebugEngine extends CacheEngine
         return call_user_func_array([$this->_engine, 'config'], func_get_args());
     }
 
-    /*
-    * Returns the config.
-    *
-    * @param string|null $key The key to get or null for the whole config.
-    * @param mixed $default The return value when the key does not exist.
-    * @return mixed Config value being read.
-    */
+    /**
+     * Returns the config.
+     *
+     * @param string|null $key The key to get or null for the whole config.
+     * @param mixed $default The return value when the key does not exist.
+     * @return mixed Config value being read.
+     */
     public function getConfig($key = null, $default = null)
     {
         return $this->_engine->getConfig($key, $default);
     }
 
-    /*
-    * Sets the config.
-    *
-    * @param string|array $key The key to set, or a complete array of configs.
-    * @param mixed|null $value The value to set.
-    * @param bool $merge Whether to recursively merge or overwrite existing config, defaults to true.
-    * @return $this
-    * @throws \Cake\Core\Exception\Exception When trying to set a key that is invalid.
-    */
+    /**
+     * Sets the config.
+     *
+     * @param string|array $key The key to set, or a complete array of configs.
+     * @param mixed|null $value The value to set.
+     * @param bool $merge Whether to recursively merge or overwrite existing config, defaults to true.
+     * @return $this
+     * @throws \Cake\Core\Exception\Exception When trying to set a key that is invalid.
+     */
     public function setConfig($key, $value = null, $merge = true)
     {
         return $this->_engine->setConfig($key, $value, $merge);

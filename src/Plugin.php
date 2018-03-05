afc3b1554068b20711e5e6a9edff18040ea4a3a0
Robert Pustułka <robert.pustulka@gmail.com>
2018-03-05 10:42:52 +0100

    Add plugin class.

---
+++ src/Plugin.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * CakePHP(tm) : Rapid Development Framework (https://cakephp.org)
+ * Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
+ * @link          https://cakephp.org CakePHP(tm) Project
+ * @since         DebugKit 3.15.0
+ * @license       https://www.opensource.org/licenses/mit-license.php MIT License
+ */
+namespace DebugKit;
+
+use Cake\Core\BasePlugin;
+
+/**
+ * Plugin class for CakePHP 3.6.0 plugin collection.
+ */
+class Plugin extends BasePlugin
+{
+}

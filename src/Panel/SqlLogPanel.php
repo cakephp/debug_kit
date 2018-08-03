9cc5f84e1034d74913c6660022211c2f7b13900d
Nicolas <nicolas.girardet@gmail.com>
2018-08-03 10:38:08 +0200

    Fix issue #624

--- src/Panel/SqlLogPanel.php
+++ src/Panel/SqlLogPanel.php
@@ -64,7 +64,9 @@ class SqlLogPanel extends DebugPanel
             $logger = new DebugLog($logger, $name, $includeSchemaReflection);
 
             $connection->logQueries(true);
-            $connection->setLogger($logger);
+            if (method_exists($connection, 'setLogger')) {
+                $connection->setLogger($logger);
+            }
             $this->_loggers[] = $logger;
         }
     }

e3147175e8f707bed0274f4f0392af0864e26c98
D4rkMindz <bjoern.pfoster@gmail.com>
2019-04-04 12:22:27 +0200

    Improved documentation

--- webroot/js/toolbar.js
+++ webroot/js/toolbar.js
@@ -92,13 +92,20 @@ if (elem) {
     };
   };
 
-  // Bind on ready callback.
+  // Bind on ready callbacks to DOMContentLoaded (native js) and turbolinks:load
+  // Turbolinks replaces the body and merges the head of an HTML page.
+  // Since the body is already loaded (DOMContentLoaded), the event is not triggered.
   if (doc.addEventListener) {
+    // This ensures that all event listeners get applied only once.
     if (!window.__debugKitListenersApplied) {
+      // The DOMContentLoaded is for all pages that do not have Turbolinks
       doc.addEventListener('DOMContentLoaded', onReady, false);
       doc.addEventListener('DOMContentLoaded', proxyAjaxOpen, false);
       doc.addEventListener('DOMContentLoaded', proxyAjaxSend, false);
 
+      // turbolinks:load is the alternative DOMContentLoaded Event of Turbolinks
+      // https://github.com/turbolinks/turbolinks
+      // https://github.com/cakephp/debug_kit/pull/664
       doc.addEventListener('turbolinks:load', onReady, false);
       doc.addEventListener('turbolinks:load', proxyAjaxOpen, false);
       doc.addEventListener('turbolinks:load', proxyAjaxSend, false);

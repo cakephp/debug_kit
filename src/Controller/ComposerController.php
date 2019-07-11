4a7cbd4919e5660e7dfab9ae59cebdc564a0011f
Mark Story <mark@mark-story.com>
2019-07-10 23:21:37 -0400

    Add tests for composer controller and fix deprecation.
    Add some coverage to the composer controller.

--- src/Controller/ComposerController.php
+++ src/Controller/ComposerController.php
@@ -65,7 +65,7 @@ class ComposerController extends Controller
         $input = new ArrayInput([
             'command' => 'outdated',
             '--no-interaction' => true,
-            '--direct' => filter_var($this->request->data('direct'), FILTER_VALIDATE_BOOLEAN),
+            '--direct' => filter_var($this->request->getData('direct'), FILTER_VALIDATE_BOOLEAN),
         ]);
 
         $output = $this->executeComposerCommand($input);
---
+++ tests/TestCase/Controller/ComposerControllerTest.php
@@ -0,0 +1,59 @@
+<?php
+/**
+ * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
+ * Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
+ * @link          http://cakephp.org CakePHP(tm) Project
+ * @since         3.19.1
+ * @license       http://www.opensource.org/licenses/mit-license.php MIT License
+ */
+namespace DebugKit\Test\TestCase\Controller;
+
+use Cake\Database\Driver\Sqlite;
+use Cake\Datasource\ConnectionManager;
+use Cake\Routing\RouteBuilder;
+use Cake\Routing\Router;
+use Cake\TestSuite\IntegrationTestCase;
+
+/**
+ * Composer controller test.
+ */
+class ComposerControllerTest extends IntegrationTestCase
+{
+    /**
+     * Setup method.
+     *
+     * @return void
+     */
+    public function setUp()
+    {
+        parent::setUp();
+        Router::plugin('DebugKit', function (RouteBuilder $routes) {
+            $routes->connect('/composer/:action', ['controller' => 'Composer']);
+        });
+        $this->useHttpServer(true);
+    }
+
+    /**
+     * tests index page returns as JSON
+     *
+     * @return void
+     */
+    public function testCheckDependencies()
+    {
+        $this->configRequest([
+            'headers' => [
+                'accept' => 'application/json, text/javascript, */*; q=0.01',
+            ]
+        ]);
+        $this->post('/debug_kit/composer/checkDependencies');
+        $this->assertResponseOk();
+        $this->assertContentType('application/json');
+        $data = json_decode((string)$this->_response->getBody(), true);
+        $this->assertArrayHasKey('packages', $data);
+    }
+}

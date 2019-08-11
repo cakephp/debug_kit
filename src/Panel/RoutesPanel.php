bbecb01ee0a05114302f8d18f05f6e60c2cdbbd4
Mark Story <mark@mark-story.com>
2019-08-10 21:57:36 -0400

    Fix inverted condition

--- src/Panel/RoutesPanel.php
+++ src/Panel/RoutesPanel.php
@@ -37,7 +37,7 @@ class RoutesPanel extends DebugPanel
         }
 
         $routes = array_filter(Router::routes(), function ($route) {
-            return $route->defaults['plugin'] === 'DebugKit';
+            return $route->defaults['plugin'] !== 'DebugKit';
         });
 
         return count($routes);

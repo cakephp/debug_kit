fc0182e5c52769c69541cdc6b969efe2b5a1cb84
Erwane Breton <erwane@phea.fr>
2020-02-03 09:28:11 +0100

    DebugKitController skip authorization

--- tests/TestCase/Controller/DebugKitControllerTest.php
+++ tests/TestCase/Controller/DebugKitControllerTest.php
@@ -10,11 +10,12 @@ declare(strict_types=1);
  *
  * @copyright     Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
  * @link          http://cakephp.org CakePHP(tm) Project
- * @since         3.19.1
  * @license       http://www.opensource.org/licenses/mit-license.php MIT License
  */
 namespace DebugKit\Test\TestCase\Controller;
 
+use Authorization\AuthorizationService;
+use Authorization\Policy\OrmResolver;
 use Cake\Core\Configure;
 use Cake\Event\Event;
 use Cake\Http\Response;
@@ -52,5 +53,28 @@ class DebugKitControllerTest extends IntegrationTestCase
         } finally {
             Configure::write('debug', $oldStatus);
         }
+    }
+
+    /**
+     * tests authorization is checked to avoid
+     * AuthorizationRequiredException throwned
+     *
+     * @return void
+     */
+    public function testSkipAuthorization()
+    {
+        $request = new ServerRequest(['url' => '/debug-kit/']);
+
+        $resolver = new OrmResolver();
+        $authorization = new AuthorizationService($resolver);
+
+        $request = $request->withAttribute('authorization', $authorization);
+
+        $controller = new DebugKitController($request, new Response());
+        $event = new Event('testing');
+
+        $controller->beforeFilter($event);
+
+        $this->assertTrue($controller->getRequest()->getAttribute('authorization')->authorizationChecked());
     }
 }

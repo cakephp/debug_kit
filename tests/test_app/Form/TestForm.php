5bdd18f002bc0b7763da011d3126f7618eb05bbb
Mark Story <mark@mark-story.com>
2020-11-06 22:26:52 -0500

    Fix deprecated method usage.

--- tests/test_app/Form/TestForm.php
+++ tests/test_app/Form/TestForm.php
@@ -31,7 +31,7 @@ class TestForm extends Form
      * @param \Cake\Validation\Validator $validator to use against the form
      * @return \Cake\Validation\Validator
      */
-    protected function _buildValidator(Validator $validator)
+    public function validationDefault(Validator $validator): Validator
     {
         return $validator
             ->requirePresence('accept')

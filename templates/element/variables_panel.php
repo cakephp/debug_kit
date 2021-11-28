d10154a19db7ac020ad5fdfb50f9ca9a2f90b58a
Kevin Pfeifer <kevin.pfeifer@sunlime.at>
2021-11-28 20:09:44 +0100

    adjust CSS to support multile text in info and warning paragraphs

--- templates/element/variables_panel.php
+++ templates/element/variables_panel.php
@@ -26,9 +26,9 @@ endif;
 
 if (isset($varsMaxDepth)) {
     $msg = sprintf(__d('debug_kit', '%s levels of nested data shown.'), $varsMaxDepth);
-    $msg .= __d('debug_kit', 'You can overwrite this via the config key');
-    $msg .= ' <strong>DebugKit.variablesPanelMaxDepth</strong>';
-    $msg .= __d('debug_kit', ' | Increasing the depth value can lead to an out of memory error.');
+    $msg .= ' ' . __d('debug_kit', 'You can overwrite this via the config key');
+    $msg .= ' <strong>DebugKit.variablesPanelMaxDepth</strong><br>';
+    $msg .= __d('debug_kit', 'Increasing the depth value can lead to an out of memory error.');
     printf('<p class="info">%s</p>', $msg);
 }
 

6918583f3f70e2f0ac508ee88718103bca343a4a
Kevin Pfeifer <kevin.pfeifer@sunlime.at>
2021-12-11 17:17:45 +0100

    add ability to filter plugin routes in routes tab

--- templates/element/routes_panel.php
+++ templates/element/routes_panel.php
@@ -3,13 +3,44 @@
  * @var \Cake\Routing\Route\Route[] $routes
  * @var string $matchedRoute
  */
+
+use Cake\Core\Plugin as CorePlugin;
 use Cake\Utility\Hash;
 
 $routes = Cake\Routing\Router::routes();
+
+$amount_of_routes_per_group = [];
+foreach($routes as $route){
+    if(empty($route->defaults['plugin'])) {
+        if(!array_key_exists('app', $amount_of_routes_per_group)) $amount_of_routes_per_group['app'] = 0;
+        $amount_of_routes_per_group['app']++;
+    } else {
+        if(!array_key_exists($route->defaults['plugin'], $amount_of_routes_per_group)) {
+            $amount_of_routes_per_group[$route->defaults['plugin']] = 0;
+        }
+        $amount_of_routes_per_group[$route->defaults['plugin']]++;
+    }
+}
+
+$plugin_names = [];
+foreach (CorePlugin::loaded() as $plugin_name) {
+    if(!empty($amount_of_routes_per_group[$plugin_name])){
+        $name = sprintf('%s (%s)', $plugin_name, $amount_of_routes_per_group[$plugin_name]);
+        $plugin_names[$name] = preg_replace('/\W+/','',strtolower($plugin_name));
+    }
+}
+
 ?>
-<button type="button" class="btn-primary" id="toggle-debugkit-routes">
-    <?= __d('debug_kit', 'Toggle debugkit internal routes') ?>
-</button>
+<div class="debugkit-plugin-routes-button-wrapper">
+    <button type="button" class="btn-primary js-debugkit-toggle-plugin-route" data-plugin=".route-entry--app">
+        <?= __d('debug_kit', 'App') ?><?= (!empty($amount_of_routes_per_group['app'])) ? '(' . $amount_of_routes_per_group['app'] . ')' : '' ?>
+    </button>
+    <?php foreach($plugin_names as $plugin_name => $parsed_name): ?>
+        <button type="button" class="btn-primary js-debugkit-toggle-plugin-route<?= (strpos($plugin_name, 'DebugKit') === 0) ? ' is-active' : '' ?>" data-plugin=".route-entry--plugin-<?= $parsed_name ?>">
+            <?= $plugin_name ?>
+        </button>
+    <?php endforeach; ?>
+</div>
 <table cellspacing="0" cellpadding="0" class="debug-table">
     <thead>
     <tr>
@@ -20,13 +51,20 @@ $routes = Cake\Routing\Router::routes();
     </thead>
     <tbody>
     <?php foreach ($routes as $route): ?>
-        <?php 
+        <?php
         $class = '';
-        if ($matchedRoute === $route->template):
-            $class = 'highlighted';
-        elseif ($route->defaults['plugin'] === 'DebugKit'):
-            $class = 'debugkit-route hidden';
+        if (empty($route->defaults['plugin'])):
+            $class = 'route-entry route-entry--app';
+        else:
+            $class = 'route-entry route-entry--plugin route-entry--plugin-' . preg_replace('/\W+/','',strtolower($route->defaults['plugin']));
+
+            // Hide DebugKit internal routes by default
+            if($route->defaults['plugin'] === 'DebugKit') $class .= ' hidden';
         endif;
+
+        // Highlight current route
+        if ($matchedRoute === $route->template) $class .= ' highlighted';
+
         ?>
         <tr class="<?= $class ?>">
             <td><?= h(Hash::get($route->options, '_name', $route->getName())) ?></td>
@@ -38,11 +76,25 @@ $routes = Cake\Routing\Router::routes();
 </table>
 
 <script>
-$(document).ready(function() {
-    $('#toggle-debugkit-routes').on('click', function (event) {
-        event.preventDefault();
-        var routes = $('.debugkit-route');
-        routes.toggleClass('hidden');
+    $(document).ready(function() {
+        $('#toggle-debugkit-routes').on('click', function (event) {
+            event.preventDefault();
+            var routes = $('.debugkit-route');
+            routes.toggleClass('hidden');
+        });
+
+        $('.js-debugkit-toggle-plugin-route').on('click', function (event) {
+            var $this = $(this);
+            var plugin = $this.attr('data-plugin');
+
+            if($this.hasClass('is-active')) {
+                $this.removeClass('is-active');
+                $('.route-entry' + plugin).removeClass('hidden');
+            } else {
+                $this.addClass('is-active');
+                $('.route-entry' + plugin).addClass('hidden');
+            }
+
+        });
     });
-});
 </script>

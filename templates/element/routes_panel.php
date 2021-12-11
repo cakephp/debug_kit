5e6ba41f1444aebfaba3764bb1cbe4e592c1b82d
stickler-ci <support@stickler-ci.com>
2021-12-11 16:25:33 +0000

    Fixing style errors.

--- templates/element/routes_panel.php
+++ templates/element/routes_panel.php
@@ -10,12 +10,14 @@ use Cake\Utility\Hash;
 $routes = Cake\Routing\Router::routes();
 
 $amount_of_routes_per_group = [];
-foreach($routes as $route){
-    if(empty($route->defaults['plugin'])) {
-        if(!array_key_exists('app', $amount_of_routes_per_group)) $amount_of_routes_per_group['app'] = 0;
+foreach ($routes as $route) {
+    if (empty($route->defaults['plugin'])) {
+        if (!array_key_exists('app', $amount_of_routes_per_group)) {
+            $amount_of_routes_per_group['app'] = 0;
+        }
         $amount_of_routes_per_group['app']++;
     } else {
-        if(!array_key_exists($route->defaults['plugin'], $amount_of_routes_per_group)) {
+        if (!array_key_exists($route->defaults['plugin'], $amount_of_routes_per_group)) {
             $amount_of_routes_per_group[$route->defaults['plugin']] = 0;
         }
         $amount_of_routes_per_group[$route->defaults['plugin']]++;
@@ -24,19 +26,19 @@ foreach($routes as $route){
 
 $plugin_names = [];
 foreach (CorePlugin::loaded() as $plugin_name) {
-    if(!empty($amount_of_routes_per_group[$plugin_name])){
+    if (!empty($amount_of_routes_per_group[$plugin_name])) {
         $name = sprintf('%s (%s)', $plugin_name, $amount_of_routes_per_group[$plugin_name]);
-        $plugin_names[$name] = preg_replace('/\W+/','',strtolower($plugin_name));
+        $plugin_names[$name] = preg_replace('/\W+/', '', strtolower($plugin_name));
     }
 }
 
 ?>
 <div class="debugkit-plugin-routes-button-wrapper">
     <button type="button" class="btn-primary js-debugkit-toggle-plugin-route" data-plugin=".route-entry--app">
-        <?= __d('debug_kit', 'App') ?><?= (!empty($amount_of_routes_per_group['app'])) ? ' (' . $amount_of_routes_per_group['app'] . ')' : '' ?>
+        <?= __d('debug_kit', 'App') ?><?= !empty($amount_of_routes_per_group['app']) ? ' (' . $amount_of_routes_per_group['app'] . ')' : '' ?>
     </button>
-    <?php foreach($plugin_names as $plugin_name => $parsed_name): ?>
-        <button type="button" class="btn-primary js-debugkit-toggle-plugin-route<?= (strpos($plugin_name, 'DebugKit') === 0) ? ' is-active' : '' ?>" data-plugin=".route-entry--plugin-<?= $parsed_name ?>">
+    <?php foreach ($plugin_names as $plugin_name => $parsed_name) : ?>
+        <button type="button" class="btn-primary js-debugkit-toggle-plugin-route<?= strpos($plugin_name, 'DebugKit') === 0 ? ' is-active' : '' ?>" data-plugin=".route-entry--plugin-<?= $parsed_name ?>">
             <?= $plugin_name ?>
         </button>
     <?php endforeach; ?>
@@ -50,20 +52,24 @@ foreach (CorePlugin::loaded() as $plugin_name) {
     </tr>
     </thead>
     <tbody>
-    <?php foreach ($routes as $route): ?>
+    <?php foreach ($routes as $route) : ?>
         <?php
         $class = '';
-        if (empty($route->defaults['plugin'])):
+        if (empty($route->defaults['plugin'])) :
             $class = 'route-entry route-entry--app';
-        else:
-            $class = 'route-entry route-entry--plugin route-entry--plugin-' . preg_replace('/\W+/','',strtolower($route->defaults['plugin']));
+        else :
+            $class = 'route-entry route-entry--plugin route-entry--plugin-' . preg_replace('/\W+/', '', strtolower($route->defaults['plugin']));
 
             // Hide DebugKit internal routes by default
-            if($route->defaults['plugin'] === 'DebugKit') $class .= ' hidden';
+            if ($route->defaults['plugin'] === 'DebugKit') {
+                $class .= ' hidden';
+            }
         endif;
 
         // Highlight current route
-        if ($matchedRoute === $route->template) $class .= ' highlighted';
+        if ($matchedRoute === $route->template) {
+            $class .= ' highlighted';
+        }
 
         ?>
         <tr class="<?= $class ?>">

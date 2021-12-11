d65a58bfdee94117ee0d1b92af9801c570b14cd6
Kevin Pfeifer <kevin.pfeifer@sunlime.at>
2021-12-11 17:28:27 +0100

    fix sticker CI problems

--- templates/element/routes_panel.php
+++ templates/element/routes_panel.php
@@ -33,10 +33,13 @@ foreach (CorePlugin::loaded() as $plugin_name) {
 ?>
 <div class="debugkit-plugin-routes-button-wrapper">
     <button type="button" class="btn-primary js-debugkit-toggle-plugin-route" data-plugin=".route-entry--app">
-        <?= __d('debug_kit', 'App') ?><?= (!empty($amount_of_routes_per_group['app'])) ? ' (' . $amount_of_routes_per_group['app'] . ')' : '' ?>
+        <?= __d('debug_kit', 'App') ?>
+        <?= (!empty($amount_of_routes_per_group['app'])) ? ' (' . $amount_of_routes_per_group['app'] . ')' : '' ?>
     </button>
     <?php foreach($plugin_names as $plugin_name => $parsed_name): ?>
-        <button type="button" class="btn-primary js-debugkit-toggle-plugin-route<?= (strpos($plugin_name, 'DebugKit') === 0) ? ' is-active' : '' ?>" data-plugin=".route-entry--plugin-<?= $parsed_name ?>">
+        <button type="button" class="btn-primary js-debugkit-toggle-plugin-route
+            <?= (strpos($plugin_name, 'DebugKit') === 0) ? ' is-active' : '' ?>"
+            data-plugin=".route-entry--plugin-<?= $parsed_name ?>">
             <?= $plugin_name ?>
         </button>
     <?php endforeach; ?>
@@ -56,7 +59,8 @@ foreach (CorePlugin::loaded() as $plugin_name) {
         if (empty($route->defaults['plugin'])):
             $class = 'route-entry route-entry--app';
         else:
-            $class = 'route-entry route-entry--plugin route-entry--plugin-' . preg_replace('/\W+/','',strtolower($route->defaults['plugin']));
+            $class = 'route-entry route-entry--plugin route-entry--plugin-' .
+                preg_replace('/\W+/','',strtolower($route->defaults['plugin']));
 
             // Hide DebugKit internal routes by default
             if($route->defaults['plugin'] === 'DebugKit') $class .= ' hidden';
